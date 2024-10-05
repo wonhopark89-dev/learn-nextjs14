fd15e8533882a48731f21379d302ef4bd66965d8
wonhopark89-dev <wonhopark89.dev@gmail.com>
2024-10-05 23:33:09 +0900

    reset

--- app/(movies)/movies/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function MovieDetail({ params: { id } }: { params: { id: string } }) {
-  console.log('movie detail', id);
-  return (
-    <div>
-      <h1>{'movie detail'} </h1>
-    </div>
-  );
-}
