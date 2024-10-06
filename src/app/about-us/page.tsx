94aa15c58a1acb3e1a4983369a57e1d0d95b18ed
wonhopark89-dev <wonhopark89.dev@gmail.com>
2024-10-06 19:54:47 +0900

    metadata

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'About Us',
+};
+
 export default function AboutUs() {
   return <h1>About Us</h1>;
 }
--- src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import Navigation from '@/components/navigation';
-
-export default function Home() {
-  return <div>Hello</div>;
-}
