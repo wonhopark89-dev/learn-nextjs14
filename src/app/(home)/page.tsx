b28c4612ff0a6a27094ec13a4e443ede2daa8cae
wonhopark89-dev <wonhopark89.dev@gmail.com>
2024-10-07 02:30:41 +0900

    client side

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,20 @@
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'Home',
-};
+'use client';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
-  return <div>Hello</div>;
+  const [isLoading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    const response = await fetch('https://nomad-movies.nomadcoders.workers.dev/movies');
+    const json = await response.json();
+    setMovies(json);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return <div>{isLoading ? 'Loading... ' : JSON.stringify(movies)}</div>;
 }
---
+++ src/app/(movies)/movies/[id]/page.tsx
@@ -0,0 +1,3 @@
+export default function MovieDetail({ params: { id } }: { params: { id: string } }) {
+  return <h1>Movies {id}</h1>;
+}
