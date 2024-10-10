af5c6ace8be4bd0d72d6710d3304f8c5300decf4
wonhopark89-dev <wonhopark89.dev@gmail.com>
2024-10-11 02:10:26 +0900

    loading and parallrel requests

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,16 +4,24 @@ export const metadata: Metadata = {
   title: 'Home',
 };
 
-const URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
+export const API_URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
 
 const getMovies = async () => {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  const response = await fetch(URL);
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const response = await fetch(API_URL);
   const json = await response.json();
   return json;
 };
 
 export default async function Home() {
   const movies = await getMovies();
-  return <div>{JSON.stringify(movies)}</div>;
+  return (
+    <div>
+      {movies.map((movie: any) => (
+        <a href={`/movies/${movie.id}`}>
+          <li key={movie.id}>{movie.title}</li>
+        </a>
+      ))}
+    </div>
+  );
 }
--- src/app/(movies)/movies/[id]/page.tsx
+++ src/app/(movies)/movies/[id]/page.tsx
@@ -1,3 +1,26 @@
-export default function MovieDetail({ params: { id } }: { params: { id: string } }) {
-  return <h1>Movies {id}</h1>;
+import { API_URL } from '@/app/(home)/page';
+
+async function getMovie(id: string) {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  const response = await fetch(`${API_URL}/${id}`);
+  const json = await response.json();
+  return json;
+}
+
+async function getVideos(id: string) {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  const json = await response.json();
+  return json;
+}
+
+export default async function MovieDetail({ params: { id } }: { params: { id: string } }) {
+  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
+
+  return (
+    <div>
+      <h1>{movie.title}</h1>
+      <h4>{JSON.stringify(videos, null, 2)}</h4>
+    </div>
+  );
 }
