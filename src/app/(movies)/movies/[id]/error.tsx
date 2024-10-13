3f6e25b96346ddac3482bb135db5ed0c35a58de4
wonhopark89-dev <wonhopark89.dev@gmail.com>
2024-10-14 02:31:21 +0900

    error handling

---
+++ src/app/(movies)/movies/[id]/error.tsx
@@ -0,0 +1,8 @@
+'use client';
+export default function Error() {
+  return (
+    <div>
+      <h1>error...</h1>
+    </div>
+  );
+}
--- src/components/movie-info.tsx
+++ src/components/movie-info.tsx
@@ -2,9 +2,7 @@ import { API_URL } from '@/app/(home)/page';
 
 async function getMovie(id: string) {
   await new Promise((resolve) => setTimeout(resolve, 2000));
-  const response = await fetch(`${API_URL}/${id}`);
-  const json = await response.json();
-  return json;
+  throw new Error('This is an error');
 }
 
 export default async function MovieInfo({ id }: { id: string }) {
